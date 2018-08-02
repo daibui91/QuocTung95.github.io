06ea9e3a59a5b4be0d77509d5b4a86672209afbc
QuocTung95 <40618984+QuocTung95@users.noreply.github.com>
2018-08-02 21:44:07 +0700

    Update hihi difficult.js

--- memory cards/difficult/hihi difficult.js
+++ memory cards/difficult/hihi difficult.js
@@ -5,7 +5,7 @@ var hopanh = ['f1', 'f2', 'f3','f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11',
 
 var current = null;
 var count = 0;
-var time = 40;
+var time = 50;
 let run;
 var ting = document.getElementById("ting");
 var victory = document.getElementById("victory");
@@ -77,7 +77,7 @@ $('.difficult').click(function (e) {
             $('.again').addClass('choilai');
             $('.momo').removeClass('momo-andi');
             count = 0;
-            time = 40;
+            time = 50;
             current = null;
             $('.card').css('pointer-events', 'auto');
 
@@ -177,7 +177,7 @@ function flip(card) {
 
                 clearInterval(run);
                 count = 0;
-                time = 40;
+                time = 50;
                 current = null;
                 $('.card').css('pointer-events', 'auto');
                 $('.time').addClass('stop');
